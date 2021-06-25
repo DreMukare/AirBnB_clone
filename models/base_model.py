87a320c914064234e49f7e063c7d7758a8cd2d0f
JudgeFudge19 <dremukare@gmail.com>
2021-06-25 18:26:39 +0000

    fixed pep8

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ from models import storage
 from datetime import datetime
 from uuid import uuid4
 
+
 class BaseModel:
     """
         class BaseModel
@@ -24,34 +25,36 @@ class BaseModel:
                 if key == 'id':
                     self.id = value
                 elif key == 'created_at':
-                    self.created_at = datetime
+                    self.created_at = datetime\
                         .strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 elif key == "updated_at":
-                    self.updated_at = datetime
+                    self.updated_at = datetime\
                         .strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                self.id = str(uuid4()) 
-                self.created_at = datetime.now()
-                    .strftime('%Y-%m-%dT%H:%M:%S.%f')
+                self.id = str(uuid4())
+                self.created_at = datetime\
+                    .now().strftime('%Y-%m-%dT%H:%M:%S.%f')
                 storage.new()
 
     def __str__(self):
         """
             prints a string representation of BaseClass instance
         """
-        return '[{}] ({}) <{}>'.
-            format(self.__class__.__name__, self.id, self.__dict__))
+        return '[{}] ({}) <{}>'\
+            .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
-            updates the public instance attribute updated_at with the current datetime
+            updates the public instance attribute
+            updated_at with the current datetime
         """
         storage.save()
-        self.updated_at = datetime.now()
-            .strftime('%Y-%m-%dT%H:%M:%S.%f')
+        self.updated_at = datetime\
+            .now().strftime('%Y-%m-%dT%H:%M:%S.%f')
 
     def to_dict(self):
         """
-            returns a dictionary containing all keys/values of __dict__ of the instance
+            returns a dictionary containing all
+            keys/values of __dict__ of the instance
         """
         dict = self.__dict__
         dict['__class__'] = self.__class__.__name__
