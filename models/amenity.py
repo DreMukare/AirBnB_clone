fd57269e6cb0b47a5dd6920a149f523c08d79039
Joan61 <jojohaki61@gmail.com>
2021-06-29 14:20:15 +0000

    define all common attributes

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+"""
+    module: Amenity
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        class Amenity
+        public attributes:
+            name(str): name of amenity
+    """
+    name = ""
