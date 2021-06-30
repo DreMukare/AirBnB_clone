73a8422af64b5908252da748058b3046d36ddd74
Joan61 <jojohaki61@gmail.com>
2021-06-30 13:54:02 +0000

    test for Place class

---
+++ tests/test_place.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    module contains: Tests for Place class
+"""
+from models.place import Place
+import unittest
+
+
+class TestPlace(unittest.TestCase):
+    """ tests class Place """
+    def setUp(self):
+        """ creates class instances """
+        self.place = Place()
+
+    def tearDown(self):
+        """ deletes instance """
+        del self.place
+
+    def test_attributes(self):
+        """ checks type and presence of attributes """
+        self.assertTrue(hasattr(self.place.name, self.place))
+        self.assertTrue(hasattr(self.place.city_id, self.place))
+        self.assertTrue(hasattr(self.place.user_id, self.place))
+        self.assertTrue(hasattr(self.place.description, self.place))
+        self.assertTrue(hasattr(self.place.number_rooms, self.place))
+        self.assertTrue(hasattr(self.place.number_bathrooms, self.place))
+        self.assertTrue(hasattr(self.place.max_guest, self.place))
+        self.assertTrue(hasattr(self.place.price_by_night, self.place))
+        self.assertTrue(hasattr(self.place.latitude, self.place))
+        self.assertTrue(hasattr(self.place.longitude, self.place))
+        self.assertTrue(hasattr(self.place.amenity_ids, self.place))
+
+    def test_type_of_attributes(self):
+        """ checks that attributes are the right type """
+        self.assertIsInstance(self.place.name, str)
+        self.assertIsInstance(self.place.city_id, str)
+        self.assertIsInstance(self.place.user_id, str)
+        self.assertIsInstance(self.place.description, str)
+        self.assertIsInstance(self.place.number_rooms, int)
+        self.assertIsInstance(self.place.number_bathrooms, int)
+        self.assertIsInstance(self.place.max_guest, int)
+        self.assertIsInstance(self.place.price_by_night, int)
+        self.assertIsInstance(self.place.latitude, float)
+        self.assertIsInstance(self.place.longitude, float)
+        self.assertIsInstance(self.place.amenity_ids, list)
+
+if __name__ == "__main__":
+    unittest.main()
