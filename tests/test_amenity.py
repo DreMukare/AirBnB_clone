4e4583911b738a695c9eb5af7598c8259185de35
Joan61 <jojohaki61@gmail.com>
2021-06-30 13:22:58 +0000

    Test for amenity class

---
+++ tests/test_amenity.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    module contains: Tests for Amenity class
+"""
+from models.amenity import Amenity
+import unittest
+
+
+class TestAmenity(unittest.TestCase):
+    """ tests class Amenity """
+    def setUp(self):
+        """ creates class instances """
+        self.amenity = Amenity()
+
+    def tearDown(self):
+        """ deletes instance """
+        del self.amenity
+
+    def test_attributes(self):
+        """ checks type and presence of attributes """
+        self.assertTrue(hasattr(self.amenity.name, self.amenity))
+
+    def test_type_of_attributes(self):
+        """ checks that attributes are the right type"""
+        self.assertIsInstance(self.amenity.name, str)
+
+if __name__ == "__main__":
+    unittest.main()
