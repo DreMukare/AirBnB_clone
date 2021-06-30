031f2651f279b28eb8837fd26100599832a888c6
MillicentMal <millicentmalinga19@gmail.com>
2021-06-30 11:12:26 +0000

    update

---
+++ tests/test_city.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    module contains: Tests for City class
+"""
+from models.city import City
+import unittest
+
+
+class TestCity(unittest.TestCase):
+    """ tests class City """
+    def setUp(self):
+        """ creates class instances """
+        self.city = City()
+
+    def tearDown(self):
+        """ deletes instance """
+        del self.city
+
+    def test_attributes(self):
+        """ checks type and presence of attributes """
+        self.assertTrue(hasattr(self.city.name, self.city))
+        self.assertTrue(hasattr(self.city.state_id, self.city))
+
+    def test_type_of_attributes(self):
+        """ checks that attributes are the right type """
+        self.assertIsInstance(self.city.name, str)
+        self.assertIsInstance(self.city.state_id, str)
+
+if __name__ == "__main__":
+    unittest.main()
