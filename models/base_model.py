efc32ecb43ddf55d39c9a3179e51c26e84fd5779
JudgeFudge19 <dremukare@gmail.com>
2021-07-01 17:00:13 +0000

    attempted fix

--- models/base_model.py
+++ models/base_model.py
@@ -46,7 +46,7 @@ class BaseModel:
             updates the public instance attribute
             updated_at with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
